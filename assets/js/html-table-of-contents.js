00992c1be8f41ef61bcc6d2c5904db66fbebc1c9
Sesh Sadasivam <seshrs@umich.edu>
2019-04-19 19:52:56 -0400

    Major overhaul for rapid theme creation - Split SASS styles into 'base' (core features) and 'default' (specific to default theme) - Modified sidebar toggle to use hamburger icon instead of caret. (The hamburger icon is more universally recognized as toggling sidebars.) Also moved position of this toggle to the upper right corner of the page. - Slightly increased width of sidebar. Also increased font size in table of contents. - Modified html-table-of-contents.js to create "section" structure in table of contents. This makes it possible to apply styles within a sub-section of the sidebar.

--- assets/js/html-table-of-contents.js
+++ assets/js/html-table-of-contents.js
@@ -30,6 +30,81 @@ function _getAnchorLink(headingNode) {
 }
 
 
+function _createHeadingToc(documentRef, heading, index, outputTocDiv) {
+    // If a heading already has an anchor from AnchorJS, use that
+    var href = '';
+    if (_nodeContainsAnchorChild(heading)) {
+        href = _getAnchorLink(heading);
+    }
+    else {
+        // Otherwise, create an anchor
+        href = '#toc' + index;
+        var anchor = documentRef.createElement('a');
+        anchor.setAttribute('name', 'toc' + index);
+        anchor.setAttribute('id', 'toc' + index);
+        heading.parentNode.insertBefore(anchor, heading);
+    }
+    
+    
+    var link = documentRef.createElement('a');
+    link.setAttribute('href', href);
+    link.textContent = heading.textContent;
+    
+    var div = documentRef.createElement('div');
+    div.setAttribute('class', 'primer-spec-toc-item primer-spec-toc-' + heading.tagName.toLowerCase());
+    
+    div.appendChild(link);
+    outputTocDiv.appendChild(div);
+}
+
+
+/**
+ * Generate a ToC heading and a single "section" given a list of `headings` DOM
+ *   elements starting from index `index`. The new ToC elements are placed
+ *   in `outputTocDiv`.
+ * 
+ * A "section" is defined to contain all subsequent headings with a tag-number
+ * larger than that of `headings[index]`. For instance, if
+ * `headings` contains a list of `[h1, h2, h2, h1, h3]`, the `h2` headings
+ * are included in the first `h1` section, and the `h3` heading is included in
+ * the second `h1` section. Note that all headings have their own sections,
+ * however their sections may be empty. (In this example, the non-`h1` headings
+ * all have empty sections.)
+ * As a result, this function operates in a recursive manner.
+ * @param {HTMLDOMDocument} documentRef - The `document` object.
+ * @param {Array<HTMLElement>} headings - A flat-list of heading DOM nodes.
+ * @param {number} index - The starting index into `headings`.
+ * @param {HTMLElement} outputTocDiv - The element where the heading 
+ *      and section indicated by `index` should be placed.
+ */
+function _generateToCSections(documentRef, headings, index, outputTocDiv) {
+    if (index >= headings.length) {
+        return index;
+    }
+
+    var heading = headings[index];
+    if (heading.classList.contains('primer-spec-toc-ignore')) {
+        return _generateToCSections(documentRef, headings, index + 1, outputTocDiv);
+    }
+
+    _createHeadingToc(documentRef, heading, index, outputTocDiv);
+    var sectionDiv = documentRef.createElement('div');
+    sectionDiv.setAttribute('class', 'primer-spec-toc-section');
+
+    var i = index + 1;
+    while (i < headings.length) {
+        if (headings[i].tagName[1] <= heading.tagName[1]) {
+            break;
+        }
+        else {
+            i = _generateToCSections(documentRef, headings, i, sectionDiv);
+        }
+    }
+    outputTocDiv.appendChild(sectionDiv);
+    return i;
+}
+
+
 /**
  * Generates a table of contents for your document based on the headings
  *  present. Anchors are injected into the document and the
@@ -37,8 +112,8 @@ function _getAnchorLink(headingNode) {
  *  contents will be generated inside of the first element with the id `toc`.
  * @param {HTMLDOMDocument} documentRef Optional A reference to the document
  *  object. Defaults to `document`.
+ * @author Sesh Sadasivam
  * @author Matthew Christopher Kastor-Inare III
- * @version 20130726
  * @url https://github.com/matthewkastor/html-table-of-contents
  * @example
  * // call this after the page has loaded
@@ -48,35 +123,9 @@ function htmlTableOfContents(documentRef) {
     var documentRef = documentRef || document;
     var toc = documentRef.getElementById('primer-spec-toc');
     var headings = [].slice.call(documentRef.body.querySelectorAll('h1, h2, h3, h4, h5, h6'));
-    headings.forEach(function (heading, index) {
-        // Ignore headings that should not be part of the ToC
-        if (heading.classList.contains('primer-spec-toc-ignore')) {
-            return;
-        }
 
-        // If a heading already has an anchor from AnchorJS, use that
-        var href = '';
-        if (_nodeContainsAnchorChild(heading)) {
-            href = _getAnchorLink(heading);
-        }
-        else {
-            // Otherwise, create an anchor
-            href = '#toc' + index;
-            var anchor = documentRef.createElement('a');
-            anchor.setAttribute('name', 'toc' + index);
-            anchor.setAttribute('id', 'toc' + index);
-            heading.parentNode.insertBefore(anchor, heading);
-        }
-        
-        
-        var link = documentRef.createElement('a');
-        link.setAttribute('href', href);
-        link.textContent = heading.textContent;
-        
-        var div = documentRef.createElement('div');
-        div.setAttribute('class', 'primer-spec-toc-item primer-spec-toc-' + heading.tagName.toLowerCase());
-        
-        div.appendChild(link);
-        toc.appendChild(div);
-    });
+    var index = 0;
+    while (index < headings.length) {
+        index = _generateToCSections(documentRef, headings, index, toc);
+    }
 }
