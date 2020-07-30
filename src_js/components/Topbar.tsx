139fab14633468dc7225f59ff4eb677c9fe217a0
Sesh Sadasivam <seshrs@umich.edu>
2020-07-29 20:06:24 -0400

    Add Topbar component

---
+++ src_js/components/Topbar.tsx
@@ -0,0 +1,45 @@
+import { h } from 'preact';
+import { connect } from 'redux-zero/preact';
+import IconType from './common/IconType';
+import InlineButton from './common/InlineButton';
+
+// Importing only for types
+import { BoundActions } from 'redux-zero/types/Actions';
+import actions from '../actions';
+import { StoreStateType } from '../store';
+
+type PropsType = {};
+
+function Topbar(
+  props: PropsType &
+    StoreStateType &
+    BoundActions<StoreStateType, typeof actions>,
+) {
+  return (
+    <div class="primer-spec-topbar position-fixed width-full top-0 left-0 py-2 no-print">
+      <div
+        class={`primer-spec-sidebar-toggle-fixed primer-spec-float-left ${
+          props.sidebarShown ? 'sidebar-shown' : ''
+        }`}
+      >
+        <InlineButton
+          icon={IconType.SIDEBAR}
+          onClick={props.toggleSidebarShown}
+        />
+      </div>
+      <div class="primer-spec-settings-toggle primer-spec-float-right">
+        <InlineButton
+          icon={IconType.SETTINGS}
+          onClick={props.toggleSidebarShown}
+        />
+      </div>
+      <div class="primer-spec-settings-toggle primer-spec-settings-toggle-hidden primer-spec-float-right">
+        <InlineButton icon={IconType.CLOSE} onClick={toggleSettings} />
+      </div>
+    </div>
+  );
+}
+
+function toggleSettings() {}
+
+export default connect(null, actions)(Topbar);
