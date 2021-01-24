59703c6689a746aa37a1b0820991320c6f1ec4e2
Sesh Sadasivam <seshrs@umich.edu>
2021-01-24 17:02:30 -0500

    Concat site URL with sitemap page URLs

--- src_js/components/sidebar/Sitemap.tsx
+++ src_js/components/sidebar/Sitemap.tsx
@@ -1,4 +1,5 @@
 import { Fragment, h } from 'preact';
+import Config from '../../Config';
 
 import type {
   SitemapNodePageType,
@@ -22,7 +23,7 @@ export default function Sitemap(props: PropsType): h.JSX.Element {
               sitemapNode.page.current ? 'primer-spec-toc-active' : ''
             }`}
           >
-            <a href={sitemapNode.page.url}>
+            <a href={getUrl(sitemapNode.page.url)}>
               <i class="fas fa-home" /> {sitemapNode.page.name}
             </a>
           </div>
@@ -44,7 +45,7 @@ function generateListItems(node: SitemapNodeType, level = 1): h.JSX.Element[] {
             childPage.page.current ? 'primer-spec-toc-active' : ''
           }`}
         >
-          <a href={childPage.page.url}>{childPage.page.name}</a>
+          <a href={getUrl(childPage.page.url)}>{childPage.page.name}</a>
         </div>
         <ul class="primer-spec-toc-section primer-spec-toc-list">
           {...generateListItems(childPage, level + 1)}
@@ -69,137 +70,6 @@ function generateListItems(node: SitemapNodeType, level = 1): h.JSX.Element[] {
   return [...pageItems, ...dirItems];
 }
 
-// function generateTocNodesForContentNode(
-//   contentNodeSelector: string,
-//   threshold: number,
-// ) {
-//   const contentNode = document.body.querySelector(contentNodeSelector);
-//   if (!contentNode) {
-//     throw new Error(
-//       `Primer Spec: TableOfContents: Main content node could not be found with selector: ${contentNodeSelector}`,
-//     );
-//   }
-
-//   const headings = [
-//     ...contentNode.querySelectorAll('h1, h2, h3, h4, h5, h6'),
-//   ].filter((heading) => !heading.classList.contains('primer-spec-toc-ignore'));
-
-//   // Initialize activeHeadingIndex to the last index. If there are no active
-//   // headings below the threshold, we should be highlighting this section.
-//   let activeHeadingIndex = headings.length - 1;
-//   for (let i = 0; i < headings.length; ++i) {
-//     const heading = headings[i];
-//     if (heading.getBoundingClientRect().top - threshold > 0) {
-//       activeHeadingIndex = i - 1;
-//       break;
-//     }
-//   }
-
-//   return generateTocNodes(headings, activeHeadingIndex);
-// }
-
-// /**
-//  * Generate a list of JSX elements forming a Table of Contents.
-//  *
-//  * Previous versions of Primer Spec used a custom fork of
-//  * htmlTableOfContents.js by Matthew Christopher Kastor-Inare III
-//  * (https://github.com/matthewkastor/html-table-of-contents).
-//  * The current implementation bears very little resemblance to the original
-//  * algorithm besides the structure of the final HTML output.
-//  *
-//  * @param headings List of HTML nodes representing header elements in the page
-//  * @param activeHeadingIndex The index of the active heading item. Use -1 to deactivate.
-//  */
-// function generateTocNodes(headings: Element[], activeHeadingIndex: number) {
-//   const unflattened = unflattenHeadings(headings, activeHeadingIndex);
-//   return (
-//     <ul class="primer-spec-toc-list">
-//       {unflattened.map((section) => generateTocNodesHelper(section))}
-//     </ul>
-//   );
-// }
-
-// function generateTocNodesHelper(section: HeadingsSectionType) {
-//   const heading = section.heading as HTMLElement;
-//   const headingLabel = getHeadingLabel(heading);
-//   return (
-//     <li>
-//       <div
-//         class={`primer-spec-toc-item primer-spec-toc-${heading.tagName.toLowerCase()} ${
-//           section.active ? 'primer-spec-toc-active' : ''
-//         }`}
-//       >
-//         <a href={getAnchorLink(heading)}>{headingLabel}</a>
-//       </div>
-//       <ul class="primer-spec-toc-section primer-spec-toc-list">
-//         {section.section.map((_section) => generateTocNodesHelper(_section))}
-//       </ul>
-//     </li>
-//   );
-// }
-
-// function getAnchorLink(headingNode: HTMLElement) {
-//   const anchorNode = headingNode.querySelector('a.anchorjs-link');
-//   if (!anchorNode) {
-//     console.error(
-//       'Primer Spec: getAnchorLink expected heading to contain anchor while populating table of contents.',
-//     );
-//     return '#';
-//   }
-//   return anchorNode.getAttribute('href') || '#';
-// }
-
-// // We memoize the heading label since it's not expected to change
-// // for the lifetime of the page.
-// const headingLabelMemo: { [id: string]: h.JSX.Element } = {};
-// /**
-//  * Generate a label for use in the ToC Sidebar that represents the given
-//  * `headingNode`.
-//  *
-//  * Ordinarily, `headingNode.innerText` would have been sufficient, but this
-//  * function preserves inline code-blocks in headings.
-//  *
-//  * @param headingNode The heading node for which we need to generate a label
-//  */
-// function getHeadingLabel(headingNode: HTMLElement): h.JSX.Element {
-//   if (headingNode.id && headingLabelMemo[headingNode.id]) {
-//     return headingLabelMemo[headingNode.id];
-//   }
-
-//   let headingLabel: h.JSX.Element | null = null;
-//   try {
-//     const labelComponents: h.JSX.Element[] = [];
-//     headingNode.childNodes.forEach((childNode) => {
-//       switch (childNode.nodeType) {
-//         case Node.TEXT_NODE:
-//           labelComponents.push(<Fragment>{childNode.nodeValue}</Fragment>);
-//           break;
-
-//         case Node.ELEMENT_NODE:
-//           if (!(childNode instanceof HTMLElement)) {
-//             throw new Error('getHeadingLabel expected HTML Element');
-//           }
-//           if (childNode.tagName === 'CODE' || childNode.tagName === 'TT') {
-//             labelComponents.push(
-//               <code class={[...childNode.classList].join(' ')}>
-//                 {childNode.innerText}
-//               </code>,
-//             );
-//           }
-//           break;
-//       }
-//     });
-//     headingLabel = <Fragment>{labelComponents}</Fragment>;
-//   } catch (e) {
-//     console.error(
-//       'Primer Spec ToC: Errored while building heading label for heading',
-//       headingNode,
-//     );
-//     headingLabel = <Fragment>{headingNode.innerText}</Fragment>;
-//   }
-
-//   if (headingNode.id) {
-//     headingLabelMemo[headingNode.id] = headingLabel;
-//   }
-//   return headingLabel;
-// }
+function getUrl(absoluteUrl: string): string {
+  return Config.BASE_URL + absoluteUrl;
+}
