a80912083f98fea8c56a5e7871793135c793592f
Sesh Sadasivam <seshrs@umich.edu>
2021-01-27 10:14:37 -0500

    Rename JS include-file to clarify that it is a Liquid template

--- _includes/sitemap_custom_item.liquid.js
+++ _includes/sitemap_custom_item.liquid.js
@@ -5,7 +5,7 @@
   {%- elsif include.item.pages -%}
   pages: [
     {%- for itemChild in include.item.pages -%}
-    {% include sitemap_custom_item.js item=itemChild %},
+    {% include sitemap_custom_item.liquid.js item=itemChild %},
     {%- endfor -%}
   ],
   {%- endif -%}
