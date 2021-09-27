f3173b7cfd6eb8aa34c941e5df402bd0352efe13
Sesh Sadasivam <seshrs@umich.edu>
2021-09-27 19:51:53 -0400

    Add utility to parse highlight-range inputs

--- src_js/components/main_content/__tests__/useCodeBlockGists.test.ts
+++ src_js/components/main_content/__tests__/useCodeBlockGists.test.ts
@@ -1,4 +1,6 @@
-import useCodeBlockGists from '../useCodeBlockGists';
+import useCodeBlockGists, {
+  parseGistHighlightRanges,
+} from '../useCodeBlockGists';
 
 const PYTHON_GIST = `<div class="language-console primer-spec-gist highlighter-rouge"><div class="highlight"><pre class="highlight"><code><span class="gp">$</span><span class="w"> </span>python3 <span class="nt">--version</span>  <span class="c"># NOTE: Your Python version may be different.</span>
 <span class="go">Python 3.7.4
@@ -66,6 +68,70 @@ describe('useCodeBlockGists', () => {
     expect(lineNum.getAttribute('data-line-number')).toBe('2');
     expect((row.childNodes[1] as HTMLElement).innerHTML).toBe(
       '<span class="go">Python 3.7.4</span>',
+    );
+  });
+});
+
+describe('parseGistHighlightRanges', () => {
+  test('empty string', () => {
+    expect(parseGistHighlightRanges('', 30)).toEqual(new Set());
+  });
+
+  test('invalid string', () => {
+    expect(parseGistHighlightRanges('spam and eggs', 30)).toEqual(new Set());
+  });
+
+  test('invalid comma-separated string', () => {
+    expect(parseGistHighlightRanges('spam,eggs', 30)).toEqual(new Set());
+  });
+
+  test('single line number', () => {
+    expect(parseGistHighlightRanges('24', 30)).toEqual(new Set([24]));
+  });
+
+  test('single line number out of bounds', () => {
+    expect(parseGistHighlightRanges('32', 30)).toEqual(new Set());
+  });
+
+  test('single line number at upper bound', () => {
+    expect(parseGistHighlightRanges('30', 30)).toEqual(new Set([30]));
+  });
+
+  test('single line number at lower bound', () => {
+    expect(parseGistHighlightRanges('1', 30)).toEqual(new Set([1]));
+  });
+
+  test('single range', () => {
+    expect(parseGistHighlightRanges('18-21', 30)).toEqual(
+      new Set([18, 19, 20, 21]),
+    );
+  });
+
+  test('single range completely out of bounds', () => {
+    expect(parseGistHighlightRanges('32-38', 30)).toEqual(new Set());
+  });
+
+  test('single range partially out of bounds', () => {
+    expect(parseGistHighlightRanges('28-32', 30)).toEqual(new Set());
+  });
+
+  test('single range with inverted bounds', () => {
+    expect(parseGistHighlightRanges('28-26', 30)).toEqual(new Set());
+  });
+
+  test('multiple single numbers', () => {
+    expect(parseGistHighlightRanges('4,0,6', 30)).toEqual(new Set([4, 6]));
+  });
+
+  test('multiple ranges', () => {
+    expect(parseGistHighlightRanges('14-15,24-32,12-14', 30)).toEqual(
+      new Set([12, 13, 14, 15]),
+    );
+  });
+
+  test('multiple ranges and lines', () => {
+    expect(parseGistHighlightRanges('4, 24-27, 3-5', 30)).toEqual(
+      new Set([3, 4, 5, 24, 25, 26, 27]),
     );
   });
 });
--- src_js/components/main_content/useCodeBlockGists.tsx
+++ src_js/components/main_content/useCodeBlockGists.tsx
@@ -20,8 +20,8 @@ export default function useCodeBlockGists(
   // The original structure of a codeblock:
   // <div
   //   class="primer-spec-gist highlighter-rouge language-[lang]"
-  //   data-filename="[filename]"          {/* OPTIONAL */}
-  //   data-highlights="[highlight-range]" {/* OPTIONAL */}
+  //   data-filename="[filename]"         {/* OPTIONAL */}
+  //   data-highlight="[highlight-range]" {/* OPTIONAL */}
   // >
   //   <div class="highlight">
   //     <pre class="highlight">
@@ -134,4 +134,58 @@ function getGistLanguage(gistSrc: Element): string | null {
     }
   }
   return null;
+}
+
+/**
+ * Parses a string reprenting a list of line numbers, some of which may be
+ * ranges. The parsed output is a Set of line numbers that are included in the
+ * range.
+ *
+ * For instance, the string `'13, 24-26, 25-27'` is parsed as
+ * `Set([13, 24, 25, 26, 27])`
+ *
+ * @param rawHighlightRanges A comma-separated string representing ranges
+ * @param maxLineNumber The maximum valid line number
+ */
+export function parseGistHighlightRanges(
+  rawHighlightRanges: string | null,
+  maxLineNumber: number,
+): Set<number> {
+  const highlightedLines = new Set<number>();
+  if (!rawHighlightRanges) {
+    return highlightedLines;
+  }
+
+  const ranges = rawHighlightRanges.split(',');
+  ranges.forEach((range) => {
+    // First check if it's a single number
+    const potentialLineNum = +range;
+    if (isNumWithinInclusiveRange(potentialLineNum, 1, maxLineNumber)) {
+      highlightedLines.add(potentialLineNum);
+    } else {
+      const rangeParts = range.trim().split('-');
+      if (rangeParts.length === 2) {
+        const lower = +rangeParts[0];
+        const upper = +rangeParts[1];
+        if (
+          isNumWithinInclusiveRange(lower, 1, maxLineNumber) &&
+          isNumWithinInclusiveRange(upper, 1, maxLineNumber) &&
+          lower <= upper
+        ) {
+          for (let i = lower; i <= upper; ++i) {
+            highlightedLines.add(i);
+          }
+        }
+      }
+    }
+  });
+  return highlightedLines;
+}
+
+function isNumWithinInclusiveRange(
+  num: number | null,
+  lower: number,
+  upper: number,
+): boolean {
+  return num != null && !Number.isNaN(num) && num >= lower && num <= upper;
 }
