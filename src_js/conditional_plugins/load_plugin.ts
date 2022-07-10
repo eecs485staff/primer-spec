d6d6a7fc20c2763e0669c44130e911b93d1fd573
Sesh Sadasivam <seshrs@umich.edu>
2022-07-09 23:15:50 -0400

    Add comment explaining bundle-splitting

--- src_js/conditional_plugins/load_plugin.ts
+++ src_js/conditional_plugins/load_plugin.ts
@@ -1,5 +1,14 @@
 import { Plugin } from './types.d';
 
+/**
+ * Given a plugin ID, lazy-load the appropriate JS module containing the plugin
+ * definition and return the plugin.
+ *
+ * Notice that we use the dynamic `import()` syntax. Webpack identifies this as
+ * an opportunity to split the JS bundle, hence decreasing the size of the main
+ * Primer Spec JS bundle. Additionally, we won't download the JS code for all
+ * plugins, only the ones that need to run.
+ */
 export async function loadPlugin(pluginId: string): Promise<Plugin | null> {
   let plugin: Plugin | null = null;
   switch (pluginId) {
